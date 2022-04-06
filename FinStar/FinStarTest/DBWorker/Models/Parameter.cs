a460f0f4ae2f61fd3977ecca1dcf67e5ec13667b
raderfast <unraderfast@gmail.com>
2022-04-07 04:29:34 +0900

    added Parameter model to return result of post query

---
+++ FinStar/FinStarTest/DBWorker/Models/Parameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DBWorker.Models
+{
+    public class Parameter
+    {
+        /// <summary>
+        /// Название параметра
+        /// </summary>
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Значение
+        /// </summary>
+        [JsonProperty("value", NullValueHandling = NullValueHandling.Ignore)]
+        public string Value { get; set; }
+    }
+}
