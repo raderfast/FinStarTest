07cefe5d644275b41097b607153cb6412af90af1
raderfast <unraderfast@yandex.ru>
2022-04-06 22:39:23 +0900

    init commit

---
+++ FinStar/FinStarTest/DBWorker/Context.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBWorker
+{
+    public class Context : DbContext
+    {
+        /// <summary>
+        /// Строка подключения к БД
+        /// </summary>
+        private readonly string _dbConnectionString;
+
+        #region DbSets
+
+
+
+        #endregion
+
+        /// <summary>
+        /// Конструктор контекста
+        /// </summary>
+        /// <param name="connectionString">Строка подключения</param>
+        /// <exception cref="Exception">Сообщение об ошибке</exception>
+        public Context(string connectionString)
+        {
+            try
+            {
+                _dbConnectionString = connectionString;
+
+                // Проверка наличия БД. Если БД отсутствует - создается новая.
+                Database.EnsureCreated();
+
+                if (!Database.CanConnect())
+                    throw new Exception("Возникла ошибка при подключении к БД PostgreSQL. Проверьте данные подключения и повторите попытку.");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Возникла ошибка при подключении к БД PostgreSQL. Проверьте данные подключения и повторите попытку.", ex);
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .EnableSensitiveDataLogging()
+                .UseNpgsql($"Host=localhost;Port=5432;Database=Test;Username=postgres;Password=1");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
