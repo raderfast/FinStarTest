59d52bc64182f2cdaf28ad1ee0e02ccd86ff593b
Dmitry Lomov <unraderfast@gmail.com>
2022-04-13 18:46:09 +0900

    Update Case2-2.sql

--- Queries/Case2-2.sql
+++ Queries/Case2-2.sql
@@ -2,5 +2,5 @@ SELECT "Clients"."ClientName"
 FROM public."Clients" 
 LEFT JOIN "ClientContacts" on "ClientId" = "Clients"."Id"
 GROUP BY "Clients"."Id" 
-HAVING COUNT("ClientContacts"."ClientId") > 2Dates 
+HAVING COUNT("ClientContacts"."ClientId") > 2
 ORDER BY "Clients"."Id" ASC 
